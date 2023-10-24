b978b87b127efe1d5d77ff563d6ee36c97aa5f76
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-24 10:30:16 +0200

    Update: Added libf.h to header, deleted commented main.

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,7 +1,16 @@
-/* add header 
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 13:17:40 by mbartos           #+#    #+#             */
+/*   Updated: 2023/10/24 10:07:23 by mbartos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
@@ -16,51 +25,3 @@ void	ft_bzero(void *s, size_t n)
 		i++;
 	}
 }
-
-/*
-#include <string.h>
-#include <stdio.h>
-int	main(void)
-{
-	char	str[] = "Ahoj jak se mas";
-
-	printf("%s\n", str);
-	ft_bzero(str, strlen(str));
-	printf("%s\n", str);
-	bzero(str, strlen(str));
-	printf("%s\n", str);
-}
-*/
-
-/*
-#include <string.h>
-#include <stdio.h>
-int	main(void)
-{
-	char	str[5] = {10, 11, 12, 13, 14};
-	int		i;
-
-	i = 0;
-	while (i < 5)
-	{
-		printf("%d\n", str[i++]);
-	}
-	ft_bzero(str, sizeof(str));
-	i = 0;
-	while (i < 5)
-	{
-		printf("%d\n", str[i++]);
-	}
-	str[0] = 10;
-	str[1] = 11;
-	str[2] = 12;
-	str[3] = 13;
-	str[4] = 14;
-	bzero(str, sizeof(str));
-	i = 0;
-	while (i < 5)
-	{
-		printf("%d\n", str[i++]);
-	}
-}
-*/
