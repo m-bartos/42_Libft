170e5baababee41a1a5fd1f8cc0ab5e863f7d2b7
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-24 10:49:50 +0200

    Update: Deleted commented mains in ft_tolower.c, ft_toupper.c functions

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:19:58 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/23 13:38:35 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/24 10:49:12 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,3 @@ int	ft_tolower(int c)
 		c = c + 32;
 	return (c);
 }
-
-/*
-#include <stdio.h>
-#include <ctype.h>
-int main(void)
-{
-	char	a;
-	char	b;
-
-	a = 'z';
-	b = 'z' + 1;
-	printf("%d\n", toupper(a));
-	printf("%d\n", toupper(b));
-	printf("%d\n", ft_toupper(a));
-	printf("%d\n", ft_toupper(b));
-	return (0);
-}
-*/
--- ft_toupper.c
+++ ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:20:03 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/23 13:38:39 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/24 10:49:17 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,3 @@ int	ft_toupper(int c)
 		c = c - 32;
 	return (c);
 }
-
-/*
-#include <stdio.h>
-#include <ctype.h>
-int main(void)
-{
-	char	a;
-	char	b;
-
-	a = 'z';
-	b = 'z' + 1;
-	printf("%d\n", toupper(a));
-	printf("%d\n", toupper(b));
-	printf("%d\n", ft_toupper(a));
-	printf("%d\n", ft_toupper(b));
-	return (0);
-}
-*/
