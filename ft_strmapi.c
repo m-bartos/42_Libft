b12bf75e703077b520dc40ce47e6eca4eefd7693
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-25 15:03:38 +0200

    Add: ft_striteri.c, ft_strmapi.c, ft_strtrim.c

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 10:07:03 by mbartos           #+#    #+#             */
+/*   Updated: 2023/10/25 10:45:22 by mbartos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	str = (char *) malloc ((ft_strlen(s) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 13:45:17 by mbartos           #+#    #+#             */
+/*   Updated: 2023/10/25 14:45:44 by mbartos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	is_inside(char const c, char const *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	int		s_length;
+	int		i;
+	int		start;
+
+	s_length = ft_strlen(s1);
+	while (is_inside(s1[s_length - 1], set))
+		s_length--;
+	start = 0;
+	while (is_inside(s1[start], set))
+	{
+		s_length--;
+		start++;
+	}
+	str = (char *) malloc(sizeof(char) * (s_length + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s_length)
+	{
+		str[i] = s1[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
