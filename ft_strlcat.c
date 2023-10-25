884f830c977c26a99b8b6805905242ee91297aa0
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-25 16:21:10 +0200

    Fix: ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:36:43 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:48:12 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/25 16:20:46 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	length_dest;
-	char	*source;
+	size_t		i;
+	size_t		length_dest;
+	size_t		length_src;
 
 	length_dest = ft_strlen(dest);
-	source = (char *) src;
+	length_src = ft_strlen(src);
+	if (size <= length_dest)
+		return (length_src + size);
 	i = 0;
-	while (source[i] && ((i + 1) < size))
+	while (src[i] && (length_dest + i + 1 < size))
 	{
-		dest[length_dest + i] = source[i];
+		dest[length_dest + i] = src[i];
 		i++;
 	}
-	if (size)
-		dest[length_dest + i] = 0;
-	return (ft_strlen(dest) + ft_strlen(src));
+	dest[length_dest + i] = '\0';
+	return (length_src + length_dest);
 }
