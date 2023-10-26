0172a6673f6d859969ac63797e1b6b9ab0ac3b85
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-26 18:00:47 +0200

    Fix: ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:21:32 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:07:01 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/26 18:00:27 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_calloc(size_t count, size_t size)
 	unsigned char	*memory;
 
 	memory = (unsigned char *) malloc(count * size);
-	ft_memset(memory, 0, count * size);
+	if (memory == NULL)
+		return (NULL);
+	ft_bzero(memory, count * size);
 	return ((void *) memory);
 }
