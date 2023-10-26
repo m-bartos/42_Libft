5a0b01c541c45b7c15685c17f8d6498097abf268
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-26 18:02:32 +0200

    Tests: update tests for ft_calloc and ft_strchr

--- tests/test_ft_calloc.c
+++ tests/test_ft_calloc.c
@@ -6,16 +6,19 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:46:57 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:06:57 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/26 17:16:13 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+#include<stdint.h>
+#include<stdio.h>
 
 int	main(void)
 {
 	char	*test;
 
-	test = ft_calloc(5, sizeof(int));
+	test = calloc(-5, 0);
+	printf("%p", test);
 	return (0);
 }
--- tests/test_ft_strchr.c
+++ tests/test_ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:19:36 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:09:43 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/26 16:13:09 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ int	main(void)
 	char	str[] = "ahoj ahoj ahoj";
 	char	str2[] = "bonjour";
 	char	*new_str;
+	char	*new_str2;
 
-	new_str = ft_strchr(str2, '\0');
+	new_str = ft_strchr(str2, 'b' + 256 + 128 + 128);
+	new_str2 = strchr(str2, 'b' + 256 + 128 + 128);
 	printf("Original:%s\n", str);
-	printf("Found:%s\n", new_str);
+	printf("Found ft_strchr:%s\n", new_str);
+	printf("Found    strchr:%s\n", new_str2);
 	return (0);
 }
