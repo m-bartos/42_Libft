d751895e24c6d53cbb6a66dbec121d544ce3ae4c
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-26 16:48:19 +0200

    Fix: ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:18:08 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/26 16:01:26 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/26 16:10:54 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
-
-/*
-(*str1 != 0 || *str2 != 0) && 
-#include<stdio.h>
-int	main(void)
-{
-	char s2[] = {0, 0, 127, 0};
-	char s3[] = {0, 0, 42, 0};
-	printf("%d", ft_memcmp(s2, s3, 4));
-}
-*/
