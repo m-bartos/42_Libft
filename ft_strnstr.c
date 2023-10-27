a2d0ef95aef5527540b561a7aac520751660fbf0
Martin Bartos <mbartos@c1r7s3.42prague.com>
2023-10-27 12:30:16 +0200

    Fix: ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,52 +6,47 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:19:43 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:48:30 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/27 12:29:33 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_is_str_in_str(char *str, char *to_find, size_t len)
+int	ft_is_str_in_str(const char *str, const char *to_find)
 {
 	size_t	i;
 
 	i = 0;
-	while (to_find[i] && i < len)
+	while (to_find[i])
 	{
 		if (str[i] == to_find [i])
 			i++;
 		else
 			return (0);
 	}
-	if (i == len)
-		return (0);
-	else
-		return (1);
+	return (1);
 }
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	size_t	i;
-	int		is_inside;
-	char	*str;
-	char	*to_find;
+	size_t	len_to_find;
 
-	str = (char *) big;
-	to_find = (char *) little;
-	i = 0;
+	len_to_find = ft_strlen(to_find);
 	if (to_find[0] == 0)
-		return (str);
+		return ((char *) str);
+	i = 0;
 	while (str[i] != 0 && len > 0)
 	{
 		if (str[i] == to_find[0])
 		{
-			is_inside = ft_is_str_in_str(&str[i], to_find, len);
-			if (is_inside == 1)
-				return (&str[i]);
+			if (len_to_find > len)
+				return (NULL);
+			if (ft_is_str_in_str(&str[i], to_find))
+				return ((char *) &str[i]);
 		}
 		i++;
 		len--;
 	}
-	return (0);
+	return (NULL);
 }
