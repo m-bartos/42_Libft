9c61f8a795c1660bf46ef64b0b4c76c8ccf3a245
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-30 12:59:35 +0100

    Fix: ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:19:22 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:42:29 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 12:28:32 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	new_src = src;
 	new_dest = dest;
+	if (!dest && !src)
+		return (dest);
 	if (new_dest < new_src)
 	{
 		while (n--)
