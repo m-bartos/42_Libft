d48c76f12e9187bb9794d684d7e123d959c26a9c
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-30 12:59:10 +0100

    Fix: ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:18:14 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:42:25 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 12:23:04 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const char	*new_src;
 	char		*new_dest;
 
-	new_src = src;
-	new_dest = dest;
+	new_src = (char *) src;
+	new_dest = (char *) dest;
+	if (!dest && !src)
+		return (dest);
 	while (n--)
 		*new_dest++ = *new_src++;
 	return (dest);
