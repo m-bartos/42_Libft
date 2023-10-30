1ec865527b801e7d1e2cb4f04dee445dc5bce013
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-30 12:09:58 +0100

    Clean: ft_lstdelone.c

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 13:03:49 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/30 11:40:16 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 12:09:10 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 	if (lst != NULL && del != NULL)
 	{
 		(*del)(lst->content);
-		free(lst);	
+		free(lst);
 	}
 }
