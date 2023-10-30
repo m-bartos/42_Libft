4ed875597d8493e20ca26f0f9083a5588bdc07f3
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-30 11:41:51 +0100

    Fix: ft_lstdelone.c

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 13:03:49 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/27 14:33:03 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 11:40:16 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,8 @@
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (lst != NULL && del != NULL)
+	{
 		(*del)(lst->content);
+		free(lst);	
+	}
 }
