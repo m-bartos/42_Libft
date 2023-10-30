5abe683c52d107c13124a3e01cb139ed56b568d8
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-30 11:50:59 +0100

    Tests: update test for ft_strtrim

--- tests/test_ft_strtrim.c
+++ tests/test_ft_strtrim.c
@@ -6,20 +6,22 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:45:36 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/25 14:45:59 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 11:49:59 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../libft.h"
 
+// cc -Wall -Wextra -Werror ./tests/test_ft_strtrim.c ft_strtrim.c ft_strlen.c && ./a.out 
+
 int	main(void)
 {
-	char	str[] = "ab1234ba";
-	char	set[] = "ab";
+	char	str[] = "   xxx   xxx";
+	char	set[] = " x";
 	char	*new_str;
 
 	new_str = ft_strtrim(str, set);
-	printf("%s\n", new_str);
+	printf("\nString: %s\n", new_str);
 	return (0);
 }
