5ada0da57b393b91c55cba5172a366ac3610dabf
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-30 12:58:35 +0100

    Fix: ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 13:09:43 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/26 14:24:29 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 12:55:30 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	t_list	*old_node;
+	t_list	*temp;
 	t_list	*actual_node;
 
+	if (lst == NULL || *lst == NULL || del == NULL)
+		return ;
 	actual_node = *lst;
-	while (actual_node->next != NULL)
+	while (actual_node != NULL)
 	{
-		old_node = actual_node;
-		actual_node = actual_node->next;
-		(*del)(old_node);
+		temp = actual_node->next;
+		ft_lstdelone(actual_node, del);
+		actual_node = temp;
 	}
-	(*del)(actual_node);
 	*lst = NULL;
 }
