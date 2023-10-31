d45a83c2f76de45c97f97292a01a5c808ace1bf3
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-31 08:49:48 +0100

    Clean: ft_lstsize.c

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 10:33:17 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/26 12:33:21 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/31 08:38:42 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	t_list	*actual_node;
 	size_t	length;
 
 	if (lst == NULL)
 		return (0);
-	length = 1;
-	actual_node = lst;
-	while (actual_node->next != NULL)
+	length = 0;
+	while (lst != NULL)
 	{
 		length++;
-		actual_node = actual_node->next;
+		lst = lst->next;
 	}
 	return (length);
 }
