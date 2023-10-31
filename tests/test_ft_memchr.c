bcd18a08e39a9f5448ac734d9c9d85728f4e176b
m-bartos <mbartos@c3r6s1.42prague.com>
2023-10-31 08:53:45 +0100

    Tests: update tests for ft_memchr.c and ft_substr.c

--- tests/test_ft_memchr.c
+++ tests/test_ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:18:01 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 10:08:34 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 13:53:30 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 #include <string.h>
 #include "../libft.h"
 
+// cc -Wall -Wextra -Werror ./tests/test_ft_memchr.c ft_memchr.c ft_strlen.c && ./a.out 
+
 int	main(void)
 {
 	char	str[] = "ahoj ahoj ahoj";
 	char	*new_str;
 
-	new_str = ft_memchr(str, 'h', 2);
+	new_str = ft_memchr(0x7ffde1690c60, 123, 0);
 	printf("Original:%s\n", str);
 	printf("Found ft_memchr:%s\n", new_str);
-	new_str = memchr(str, 'h', 2);
+	new_str = memchr(0x7ffde1690c60, 123, 0);
 	printf("Found memchr:%s\n", new_str);
 	return (0);
 }
--- tests/test_ft_substr.c
+++ tests/test_ft_substr.c
@@ -6,20 +6,20 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 12:01:33 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/24 13:05:39 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/10/30 13:13:19 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 #include <stdio.h>
 
-// cc -Wall -Wextra -Werror ./tests/test_ft_substr.c ft_substr.c && ./a.out 
+// cc -Wall -Wextra -Werror ./tests/test_ft_substr.c ft_substr.c ft_strlen.c && ./a.out 
 
 int	main(void)
 {
-	char	str[] = "ABCDEFGHIJKLMNOP";
+	char	str[] = "hola";
 	char	*new_str;
 
-	new_str = ft_substr(str, 5, 5);
+	new_str = ft_substr(str, 3, 1);
 	printf("%s\n", new_str);
 }
